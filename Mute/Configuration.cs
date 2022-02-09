aab0f9f0354afde2042e0a18d1c59b03d41f40a9
Aika <notaika@protonmail.com>
2022-02-09 15:44:03 -0500

    v1.1.0

--- Mute/Configuration.cs
+++ Mute/Configuration.cs
@@ -7,6 +7,8 @@ namespace Mute
     internal class Configuration
     {
         public static string AssemblyDirectory => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        public static string LogPath => Path.Combine(AssemblyDirectory, "log.txt");
+        public static string LatestPath => Path.Combine(AssemblyDirectory, "latest.txt");
 
         public static bool SpeechUseAuthToken { get; private set; }
         public static string SpeechSubscriptionKey { get; private set; }
@@ -14,11 +16,17 @@ namespace Mute
         public static string SpeechRegion { get; private set; }
         public static bool SpeechDictation { get; private set; }
         public static string SpeechLanguage { get; private set; }
+        public static bool SpeechLogging { get; private set; }
+        public static int SpeechClearAfter { get; private set; }
 
         public static bool SynthesizerEnabled { get; private set; }
         public static int SynthesizerVoice { get; private set; }
+        public static int SynthesizerOutput { get; private set; }
 
-        public static bool LogFileEnabled { get; private set; }
+        public static bool KeywordsEnabled { get; private set; }
+        public static string KeywordsType { get; private set; }
+        public static string[] KeywordsStart { get; private set; }
+        public static string[] KeywordsPause { get; private set; }
 
         static Configuration()
         {
@@ -26,24 +34,31 @@ namespace Mute
 
             bool.TryParse(iniParser.Value("Speech_UseAuthToken", "True"), out var speechUseAuthToken);
             SpeechUseAuthToken = speechUseAuthToken;
-
             SpeechSubscriptionKey = iniParser["Speech_SubscriptionKey"];
             SpeechAuthToken = iniParser["Speech_AuthToken"];
             SpeechRegion = iniParser["Speech_Region"];
-
             bool.TryParse(iniParser.Value("Speech_Dictation", "True"), out var speechDictation);
             SpeechDictation = speechDictation;
-
             SpeechLanguage = iniParser.Value("Speech_Language", "en-US");
+            bool.TryParse(iniParser.Value("Speech_Logging", "True"), out var speechLogging);
+            SpeechLogging = speechLogging;
+            int.TryParse(iniParser.Value("Speech_ClearAfter", "15000"), out var speechClearAfter);
+            SpeechClearAfter = speechClearAfter;
 
             bool.TryParse(iniParser.Value("Synthesizer_Enabled", "True"), out var synthesizerEnabled);
             SynthesizerEnabled = synthesizerEnabled;
-
             int.TryParse(iniParser.Value("Synthesizer_Voice", "0"), out var synthesizerVoice);
             SynthesizerVoice = synthesizerVoice;
+            int.TryParse(iniParser.Value("Synthesizer_Output", "0"), out var synthesizerOutput);
+            SynthesizerOutput = synthesizerOutput;
 
-            bool.TryParse(iniParser.Value("LogFile_Enabled", "True"), out bool logFileEnabled);
-            LogFileEnabled = logFileEnabled;
+            bool.TryParse(iniParser.Value("Keywords_Enabled", "False"), out var keywordsEnabled);
+            KeywordsEnabled = keywordsEnabled;
+            KeywordsType = iniParser["Keywords_Type"] ?? "StartsWith";
+            var keywordsStart = iniParser.Parse<string[]>("Keywords_Start");
+            KeywordsStart = (keywordsStart != null && keywordsStart.Length > 0) ? keywordsStart : new string[] { "Hi" };
+            var keywordsPause = iniParser.Parse<string[]>("Keywords_Pause");
+            KeywordsPause = (keywordsPause != null && keywordsPause.Length > 0) ? keywordsPause : new string[] { "Goodbye" };
         }
     }
 }
--- Mute/Program.cs
+++ Mute/Program.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.CognitiveServices.Speech;
+﻿using CSCore;
+using CSCore.MediaFoundation;
+using CSCore.SoundOut;
+using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using Mute.Providers;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using SpeechSynthesizer = System.Speech.Synthesis.SpeechSynthesizer;
 
 namespace Mute
@@ -11,12 +16,31 @@ namespace Mute
     public class Program
     {
         private static SpeechSynthesizer _synthesizer = new();
+        private static Timer _clear;
+        private static bool _disabled;
 
         public static void Main(string[] args)
             => MainAsync(args)
                 .ConfigureAwait(false)
                 .GetAwaiter()
                 .GetResult();
+
+        private static void Speak(string value)
+        {
+            using (var stream = new MemoryStream())
+            {
+                _synthesizer.SetOutputToWaveStream(stream);
+                _synthesizer.Speak(value);
+
+                using (var output = new WaveOut { Device = new WaveOutDevice(Configuration.SynthesizerOutput) })
+                using (var source = new MediaFoundationDecoder(stream))
+                {
+                    output.Initialize(source);
+                    output.Play();
+                    output.WaitForStopped();
+                }
+            }
+        }
 
         private static SpeechConfig CreateConfigFromResources()
         {
@@ -26,6 +50,15 @@ namespace Mute
 
         public static async Task MainAsync(string[] args)
         {
+            Console.WriteLine("Outputs:");
+            var devices = WaveOutDevice.EnumerateDevices();
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"{device.DeviceId} > {device.Name}");
+            }
+
+            Console.WriteLine($"Selected output: {devices.Where(x => x.DeviceId == Configuration.SynthesizerOutput).First().Name}");
+
             Console.WriteLine("Voices:");
             var voices = _synthesizer.GetInstalledVoices();
             for (var i = 0; i < voices.Count; i++)
@@ -35,7 +68,6 @@ namespace Mute
             Console.WriteLine($"Selected voice: {voice}");
 
             _synthesizer.SelectVoice(voice);
-            _synthesizer.SetOutputToDefaultAudioDevice();
 
             SpeechConfig config;
             if (Configuration.SpeechUseAuthToken)
@@ -61,6 +93,13 @@ namespace Mute
             config.SetProperty(PropertyId.SpeechServiceConnection_InitialSilenceTimeoutMs, "5000");
             config.SetProperty(PropertyId.SpeechServiceConnection_EndSilenceTimeoutMs, "5000");
 
+            if (Configuration.SpeechLogging)
+                File.WriteAllText(Configuration.LatestPath, string.Empty);
+
+            _clear = new Timer(Configuration.SpeechClearAfter);
+            _clear.Elapsed += (s, e) => File.WriteAllText(Configuration.LatestPath, string.Empty);
+            _clear.AutoReset = false;
+
             var recognizer = new SpeechRecognizer(config, AudioConfig.FromDefaultMicrophoneInput());
             recognizer.Recognized += OnRecognized;
 
@@ -76,11 +115,45 @@ namespace Mute
             {
                 Console.WriteLine($"Recognized: {e.Result.Text}");
 
-                if (Configuration.SynthesizerEnabled)
-                    _synthesizer.Speak(e.Result.Text);
+                var isStartKeyword = false;
+                if (Configuration.KeywordsType == "Contains")
+                    isStartKeyword = Configuration.KeywordsStart.Where(x => e.Result.Text.ToUpper().Contains(x.ToUpper())).Count() != 0;
+                else if (Configuration.KeywordsType == "StartsWith")
+                    isStartKeyword = Configuration.KeywordsStart.Where(x => e.Result.Text.ToUpper().StartsWith(x.ToUpper())).Count() != 0;
+                else if (Configuration.KeywordsType == "EndsWith")
+                    isStartKeyword = Configuration.KeywordsStart.Where(x => e.Result.Text.ToUpper().EndsWith(x.ToUpper())).Count() != 0;
 
-                if (Configuration.LogFileEnabled)
-                    File.AppendAllText(Path.Combine(Configuration.AssemblyDirectory, "Log.txt"), $"{e.Result.Text}\n");
+                var isPauseKeyword = false;
+                if (Configuration.KeywordsType == "Contains")
+                    isPauseKeyword = Configuration.KeywordsPause.Where(x => e.Result.Text.ToUpper().Contains(x.ToUpper())).Count() != 0;
+                else if (Configuration.KeywordsType == "StartsWith")
+                    isPauseKeyword = Configuration.KeywordsPause.Where(x => e.Result.Text.ToUpper().StartsWith(x.ToUpper())).Count() != 0;
+                else if (Configuration.KeywordsType == "EndsWith")
+                    isPauseKeyword = Configuration.KeywordsPause.Where(x => e.Result.Text.ToUpper().EndsWith(x.ToUpper())).Count() != 0;
+
+                if (isPauseKeyword && !isStartKeyword && !_disabled)
+                {
+                    Console.WriteLine("Synthesizer and Speech Logging is now disabled.");
+                    _disabled = true;
+                }
+
+                if (!_disabled && Configuration.SynthesizerEnabled)
+                    Speak(e.Result.Text);
+
+                if (!_disabled && Configuration.SpeechLogging)
+                {
+                    File.AppendAllText(Configuration.LogPath, $"{e.Result.Text}\n");
+                    File.WriteAllText(Configuration.LatestPath, e.Result.Text);
+
+                    _clear.Stop();
+                    _clear.Start();
+                }
+
+                if (isStartKeyword && !isPauseKeyword && _disabled)
+                {
+                    Console.WriteLine("Synthesizer and Speech Logging is now enabled.");
+                    _disabled = false;
+                }
             }
         }
     }
